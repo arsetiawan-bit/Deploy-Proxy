4f8d5b6cfa2734a8769e0c722184d7ae6a276f76
arsetiawan-bit set <rudyinsania@gmail.com>
2024-08-06 20:02:20 +0000

    Update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xcad67B465cAAE170f11Ae85383c9761A1E6865d5'
+export const ProxyAdmin = '0x6205012a6fE650C35cd9f23f02F7B07754707387'
+export const SWTRImplementation = '0x8c3216c44A6a2d6A49b5e6F5ea771c7FBc753667'
     
